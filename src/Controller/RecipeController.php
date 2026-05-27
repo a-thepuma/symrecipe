5b48c8c490a1c965017c3e4dfc700cc94f488cb4
André <111359909+A-thepuma@users.noreply.github.com>
2026-05-27 16:32:37 +0200

    Partie 3 : CRUD des recettes- Créer une recette

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\RecipeType;
+use Knp\Component\Pager\PaginatorInterface;
 
 final class RecipeController extends AbstractController
 {
@@ -19,6 +24,32 @@ final class RecipeController extends AbstractController
 
         return $this->render('pages/recipe/index.html.twig', [
             'recipes' => $recipes,
+        ]);
+    }
+
+    #[Route('/recipe/nouveau', name: 'recipe.new')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $recipe = new Recipe();
+
+        $form = $this->createForm(RecipeType::class, $recipe);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($recipe);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre recette a été créée avec succès !'
+            );
+
+            return $this->redirectToRoute('app_recipe');
+        }
+
+        return $this->render('pages/recipe/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
